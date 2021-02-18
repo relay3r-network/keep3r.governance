662271c3e6c83e0ed6d0220c415207d88def059f
akshaynexus <akshaycm@hotmail.com>
2021-02-18 11:52:15 +0300

    Remove BSC Support

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ const config = {
   githubAPI: process.env.REACT_APP_GITHUB_API,
   etherscanAPI: process.env.REACT_APP_ETHERSCAN_API,
   etherscanAPIKey: process.env.REACT_APP_ETHERSCAN_KEY,
-  supportedChainIDs: [1, 56],
+  supportedChainIDs: [1],
   liquidityAddress: {
     1:  "0x375Da3e307Ef2E1A9D9e1516f80738Ca52cb7B85",
     56: "0x375Da3e307Ef2E1A9D9e1516f80738Ca52cb7B85",
@@ -19,7 +19,6 @@ const config = {
     1:  "0x0E3EF895c59E7Db27214AB5bbf56347cE115A3f4",
     56: "0x04CF6dD02688849C9A91e61317A1bCE2c2e192fE",
   },
-
   keeperAddress: {
     1:  "0x0E3EF895c59E7Db27214AB5bbf56347cE115A3f4",
     56: "0x04CF6dD02688849C9A91e61317A1bCE2c2e192fE",
@@ -31,7 +30,6 @@ const config = {
     1:  "0x183659D5088fFeeB57504B390D3bc4905e617D39",
     56: "0x5b2b9B24C0ff4E146beB5d296FbC19a1CC4335f4"
   },
-
   swapAddress: {
     1: "0xF712977c9c201d012531085F01e168d05be590E5",
   },
